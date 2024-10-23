ed67888d48db9b316538b8fa26b72930e90bda8b
CheaIsing <cheaising07@gmail.com>
2024-10-23 09:16:59 +0700

    commit

--- assets/js/authentication.js
+++ assets/js/authentication.js
@@ -14,3 +14,40 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
 });
+
+if(document.getElementById('btn-log-in')){
+  document.getElementById('btn-log-in').onclick = () => {
+    const email = document.getElementById('login-email').value;
+    const password = document.getElementById('login-password').value;
+
+    if(email && password){
+        fetch(API_URL+"/api/login", {
+            headers: {"Content-Type": "application/json"},
+            method: "POST",
+            body: JSON.stringify({email, password})
+        })
+        .then(res=>res.json())
+        .then(json=>{
+            console.log(json);
+            let token = json.data.token;
+            localStorage.setItem('authToken', token);
+            location.href = "homepage.html";
+        })
+    }    
+}
+}
+
+function logout() {
+    localStorage.removeItem('authToken');
+    location.href = "index.html";
+}
+
+// document.getElementById('btn-sign-up').onclick = () => {
+//     const full_name = document.getElementById('register-full-name');
+//     const email = document.getElementById('register-email');
+//     const password = document.getElementById('register-password');
+//     const password_confirmation = document.getElementById('register-confirm-password');
+
+//     console.log(full_name, email, password, password_confirmation);
+    
+// }
