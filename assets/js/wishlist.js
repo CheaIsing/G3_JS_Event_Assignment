0616b7ed24aa89b26c62f0bcfb3f4d0283d8b676
CheaIsing <cheaising07@gmail.com>
2024-11-21 18:40:20 +0700

    remove apiUrl

--- assets/js/wishlist.js
+++ assets/js/wishlist.js
@@ -1,6 +1,4 @@
-// getAllEvent();
-const apiUrl = "https://mps2.chandalen.dev";
-// const token = localStorage.getItem("authToken");
+
 getAllWish();
 function getAllWish() {
     fetch(apiUrl + '/api/wishlists', {
